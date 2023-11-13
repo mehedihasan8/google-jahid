496c1cbf3e38d8d0bfe0ad99400a2f4330ed6746
Kishon Kumar Pashi <pritiu111225@gmail.com>
2023-11-13 12:12:50 +0600

    analytics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,33 +1,52 @@
-import '../styles/globals.css'
-import '../styles/app.css'
-import '../styles/about.css'
-import '../styles/card.css'
-import '../styles/contact.css'
-import '../styles/filter.css'
-import '../styles/hero.css'
-import '../styles/home.css'
-import '../styles/news.css'
-import '../styles/news-card.css'
-import '../styles/privacy-policy.css'
-import '../styles/rating.css'
-import '../styles/related.css'
-import '../styles/review.css'
-import '../styles/single-news.css'
-import '../styles/single-review.css'
-import '../styles/terms.css'
-import '../styles/footer.css'
-import '../styles/popup.css'
-import '../styles/main.css'
-import '../styles/navbar.css'
+import "../styles/globals.css";
+import "../styles/app.css";
+import "../styles/about.css";
+import "../styles/card.css";
+import "../styles/contact.css";
+import "../styles/filter.css";
+import "../styles/hero.css";
+import "../styles/home.css";
+import "../styles/news.css";
+import "../styles/news-card.css";
+import "../styles/privacy-policy.css";
+import "../styles/rating.css";
+import "../styles/related.css";
+import "../styles/review.css";
+import "../styles/single-news.css";
+import "../styles/single-review.css";
+import "../styles/terms.css";
+import "../styles/footer.css";
+import "../styles/popup.css";
+import "../styles/main.css";
+import "../styles/navbar.css";
 
-import Layout from '../Layout/Layout'
+import Layout from "../Layout/Layout";
+import Script from "next/script";
 
 function App({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <Script
+        strategy="lazyOnload"
+        src={"https://www.googletagmanager.com/gtag/js?id=G-FVKDLBQ488"}
+      />
+
+      <Script strategy="lazyOnload">
+        {`
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', 'G-FVKDLBQ488', {
+                    page_path: window.location.pathname,
+                    });
+                `}
+      </Script>
+
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
   );
 }
 
-export default App
+export default App;
