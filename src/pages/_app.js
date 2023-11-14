18c5e5743b28d67dfa1e9594898223aa51683701
Kishon Kumar Pashi <pritiu111225@gmail.com>
2023-11-14 23:26:08 +0600

    update

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -22,6 +22,13 @@ import "../styles/navbar.css";
 
 import Layout from "../Layout/Layout";
 import Script from "next/script";
+import Router from "next/router";
+import NProgress from "nprogress";
+import "nprogress/nprogress.css";
+
+Router.events.on("routeChangeStart", () => NProgress.start());
+Router.events.on("routeChangeComplete", () => NProgress.done());
+Router.events.on("routeChangeError", () => NProgress.done());
 
 function App({ Component, pageProps }) {
   return (
